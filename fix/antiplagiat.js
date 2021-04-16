032b0dfaafc4c1f82a5e4740c1de80e59101d8bd
Sugar_F0x <sugarfox.dev@gmail.com>
2021-04-16 21:26:54 +0300

    :white_check_mark: Add Antiplagiat page fix
    > Remove excess colun from table head
    > Allow work break to fix unreadable text overflow

---
+++ fix/antiplagiat.js
@@ -0,0 +1,22 @@
+/**
+ * Add break line to the first column of AP table
+ * so that file names don't overflow
+ */
+{
+const style = document.createElement('style')
+style.innerText = `
+  .table-flex-body-row > div:first-of-type {
+    word-break: break-all;
+  }
+`
+document.querySelector('head').appendChild(style)
+}
+
+/**
+ * Remove excess STATUS column header
+ */
+document.querySelector('.table-flex-head > div:nth-of-type(5)')?.remove()
+
+console.log('[LMP] Antiplagiat table fixed')
+
+export default true
--- fix/index.js
+++ fix/index.js
@@ -2,3 +2,4 @@
  * Just importing modules is enough since they are run on import
  */
 import scrollbarFix from "./scrollbar.js"
+import antiplagiat from "./antiplagiat.js"
