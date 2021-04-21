3cd55a8053a07b72976fe26be9b81bf937379b52
Sugar_F0x <sugarfox.dev@gmail.com>
2021-04-21 22:33:35 +0300

    :white_check_mark: Move Entry action buttons to the top

--- fix/antiplagiat.js
+++ fix/antiplagiat.js
@@ -27,6 +27,31 @@ if (window.location.pathname.includes('antiplagiat')) {
     children.forEach(e => table.appendChild(e))
   }
 
+  /**
+   * Move AP Entry action buttons to the top
+   * Works only on Entry Table
+   */
+  if (/^\/antiplagiat\/[0-9]+$/.test(window.location.pathname)) {
+    const pdf = document.querySelector('.btn.btn-primary.btn-sm')
+    const del = document.querySelector('.btn.btn-danger.btn-sm')
+    const card = document.querySelector('.card')
+
+    card.children[0].appendChild(pdf)
+    card.children[0].appendChild(del)
+
+    const style = document.createElement('style')
+    style.innerText = `
+      .card-heading {
+        display: flex;
+      }
+      .card-heading :nth-child(2) {
+        margin-left: auto;
+        margin-right: 10px;
+      }
+    `
+    document.querySelector('head').appendChild(style)
+  }
+
   console.log('[LMP] Antiplagiat table fixed')
 }
 
